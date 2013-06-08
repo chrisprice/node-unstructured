c690c9ec834921bc139b829d17c181a5e60e466f
Chris Price <price.c@gmail.com>
2013-06-08 08:21:56 +0100

    Add missing example2 files

---
+++ test/example2/a/B.js
@@ -0,0 +1,7 @@
+a.B = function() {
+
+};
+
+a.B.prototype.test = function() {
+    return a.C.STATIC_VALUE;
+};
---
+++ test/example2/a/C.js
@@ -0,0 +1,7 @@
+a.C = function() {
+
+};
+
+a.C.prototype.test = function() {
+    return a.b.C.STATIC_VALUE;
+};
